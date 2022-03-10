c52b05de3a6bc12aed436ba12b63157a29b5c147
Danilo Ferreira <danilo.ferreira@bandtec.com.br>
2022-03-09 21:22:02 -0300

    iniciando tela do desenho de solucao no site

--- Website institucional/index.js
+++ Website institucional/index.js
@@ -1,9 +1,9 @@
 // função para adicionar menu fixo ao rolar a página
 (() => {
-    let container = document.querySelector(".headerContainer");
+    let container = document.querySelector(".header-container");
 
     window.addEventListener("scroll", () => {
-        window.scrollY > 0 ? container.classList.add("fixedHeader")
-            : container.classList.remove("fixedHeader");
+        window.scrollY > 0 ? container.classList.add("fixed-header")
+            : container.classList.remove("fixed-header");
     })
 })();
