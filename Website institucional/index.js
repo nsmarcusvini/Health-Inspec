646e40233bef903e05c57363f7e97b6bd7e23a9f
Danilo Ferreira <danilo.ferreira@bandtec.com.br>
2022-03-11 18:09:03 -0300

    subindo segunda pagina do site com processo do serviço

--- Website institucional/index.js
+++ Website institucional/index.js
@@ -1,9 +1,12 @@
 // função para adicionar menu fixo ao rolar a página
 (() => {
     let container = document.querySelector(".header-container");
-
+    
     window.addEventListener("scroll", () => {
-        window.scrollY > 0 ? container.classList.add("fixed-header")
-            : container.classList.remove("fixed-header");
+        if (window.scrollY > 0) {
+            container.classList.add("fixed-header");
+        } else {
+            container.classList.remove("fixed-header");
+        }
     })
 })();
