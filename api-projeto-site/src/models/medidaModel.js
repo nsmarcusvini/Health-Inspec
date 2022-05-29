635d08ccafd9995bd9bd7929a22b9d309115bcb5
Danilo Ferreira <danilo.ferreira@bandtec.com.br>
2022-05-28 22:44:17 -0300

    sem progresso...

--- api-projeto-site/src/models/medidaModel.js
+++ api-projeto-site/src/models/medidaModel.js
@@ -1,20 +1,25 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario,limite_linhas) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                        from medida
-                        where fk_aquario = ${idAquario}
-                        order by id desc limit ${limite_linhas}`;
-    console.log("Executando a instrução SQL: \n"+instrucaoSql);
+    let instrucaoSql = `
+        select top 7 
+            temperatura, 
+            umidade, 
+            momento,
+            DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+        from 
+            medida
+        where 
+            fk_aquario = ${idAquario}
+        order by 
+            id 
+        desc`;
+
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select temperatura, 
+    let instrucaoSql = `select temperatura, 
                             umidade, 
                             DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
                             fk_aquario 
