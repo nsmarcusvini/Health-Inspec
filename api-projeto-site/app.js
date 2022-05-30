8385727ec6254c3bd6614b126bc2b479cd21f2ec
nsmarcusvini <marcus.souza@bandtec.com.br>
2022-05-29 21:00:40 -0300

    hospedando site pro azure

--- api-projeto-site/app.js
+++ api-projeto-site/app.js
@@ -4,7 +4,7 @@ process.env.AMBIENTE_PROCESSO = "producao";
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = 3333;
+var PORTA = 8080;
 
 var app = express();
 
